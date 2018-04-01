eea15b76e4e1169bdf17e0f8cc9c480983140288
Haofan <haofan.hou@gmail.com>
2018-04-01 02:42:12 -0300

    FirebaseHelper built

---
+++ app/src/main/java/com/enroute/enroute/DBHelper/FirebaseHelper.java
@@ -0,0 +1,111 @@
+package com.enroute.enroute.DBHelper;
+
+import com.enroute.enroute.interfaces.RestaurantCallbacks;
+import com.enroute.enroute.model.Restaurant;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Haofan.
+ */
+
+public class FirebaseHelper {
+    private DatabaseReference db;
+    private ArrayList<Restaurant> restaurants = new ArrayList<>();
+
+    public FirebaseHelper(DatabaseReference db) {
+        this.db = db;
+    }
+
+    //Save the restaurant into db
+    public Boolean saveRestaurant(Restaurant restaurant)
+    {
+        Boolean saved = null;
+        if(restaurant==null)
+        {
+            saved = false;
+        }else
+        {
+            try
+            {
+                db.child("Restaurant").push().setValue(restaurant);
+                saved =true;
+
+            }catch (DatabaseException e)
+            {
+                e.printStackTrace();
+                saved =false;
+            }
+        }
+
+        return saved;
+    }
+
+
+    //retrieve restaurant info. from the db
+    public void retrieveRestaurant(final RestaurantCallbacks restaurantCallbacks) {
+
+        db.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    Restaurant restaurant = ds.getValue(Restaurant.class);
+
+                    if (restaurant != null && restaurant.getRid() != null) {
+                        restaurants.add(restaurant);
+                    }
+
+                    ///////////
+                    System.out.println("restaurant: ");
+                    System.out.println("restaurant: "+restaurant);
+
+
+
+                }
+
+                restaurantCallbacks.onRestaurantCallback(restaurants);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    Restaurant restaurant = ds.getValue(Restaurant.class);
+
+                    if (restaurant != null && restaurant.getRid() != null) {
+                        restaurants.add(restaurant);
+                    }
+
+
+                }
+
+                restaurantCallbacks.onRestaurantCallback(restaurants);
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
