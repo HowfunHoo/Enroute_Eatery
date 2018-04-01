a7c3b8c1ec234768e8c4b28f91a779139d71bf9f
Haofan <haofan.hou@gmail.com>
2018-04-01 14:40:21 -0300

    Activity for Restaurant Recommendation created

---
+++ app/src/main/java/com/enroute/enroute/RestaurantRecommendationActivity.java
@@ -0,0 +1,14 @@
+package com.enroute.enroute;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantRecommendationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_recommendation);
+        
+    }
+}
