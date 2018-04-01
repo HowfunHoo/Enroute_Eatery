b39cea4996ee55ba0a2f97dd213feafe9e004eaf
Haofan <haofan.hou@gmail.com>
2018-04-01 02:44:34 -0300

    Callback interface for Restaurant created

---
+++ app/src/main/java/com/enroute/enroute/interfaces/RestaurantCallbacks.java
@@ -0,0 +1,15 @@
+package com.enroute.enroute.interfaces;
+
+import com.enroute.enroute.model.Restaurant;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Haofan.
+ */
+
+public interface RestaurantCallbacks {
+
+    void onRestaurantCallback(ArrayList<Restaurant> restaurants);
+
+}
