97b4197ff93791acd745790377b5af1333b46cbb
Haofan <haofan.hou@gmail.com>
2018-04-01 22:30:01 -0300

    Callback Interface for Cuisines created

--- app/src/main/java/com/enroute/enroute/RestaurantRecommendationActivity.java
+++ app/src/main/java/com/enroute/enroute/RestaurantRecommendationActivity.java
@@ -13,6 +13,7 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.enroute.enroute.Singleton.RequestQueueSingleton;
+import com.enroute.enroute.model.Cuisine;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,16 +29,85 @@ public class RestaurantRecommendationActivity extends AppCompatActivity {
     //Set the default city as Halifax (The city_id of Halifax in Zomato API is 3099)
     String city_id = "3099";
 
+    //Base Url for searching restaurants
     final String restaurant_base_url = "https://developers.zomato.com/api/v2.1/search?entity_id="
             + city_id + "&entity_type=city&start=";
 
+    //Base url for searching all cuisines in the city
+    final String cuisines_base_url = "https://developers.zomato.com/api/v2.1/cuisines?city_id=";
+
+
     //API Key for Zomato API
     final String api_key = "6cfc1a029b5b9a1d7e4b713ac61a99de";
+
+    ArrayList<Cuisine> cuisines = new ArrayList<Cuisine>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_recommendation);
+
+        String cuisines_url = cuisines_base_url.concat(city_id);
+
+        //Set JsonObjectRequest
+        JsonObjectRequest cuisineRequest = new JsonObjectRequest(Request.Method.GET, cuisines_url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //LOG
+                        Log.d("Response", response.toString());
+
+                        try{
+
+                            JSONArray cuisinesJSONArray = response.getJSONArray("cuisines");
+
+                            //LOG
+                            Log.d("cuisinesJSONArray", cuisinesJSONArray.toString());
+
+                            for (int i = 0; i < cuisinesJSONArray.length(); i++){
+
+                                Cuisine cuisine = new Cuisine();
+
+                                JSONObject cuisineData = cuisinesJSONArray.getJSONObject(i).getJSONObject("cuisine");
+
+                                cuisine.setCuisine_id(cuisineData.getInt("cuisine_id"));
+                                cuisine.setCuisine_name(cuisineData.getString("cuisine_name"));
+
+                                cuisines.add(cuisine);
+
+                                /////////
+//                                Log.d("cuisine_name" , cuisineData.getString("cuisine_name"));
+
+                            }
+
+                        }catch (JSONException e){
+                            Log.d("ERROR", "Error (JSONException): " + e.toString());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError e) {
+                        Log.d("ERROR", "Error (onErrorResponse): " + e.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("user-key", api_key);
+                params.put("Accept", "application/json");
+
+                return params;
+            }
+        };
+
+        RequestQueueSingleton.getmInstance(getApplicationContext()).addToRequestQueue (cuisineRequest);
+
+        for (int i=0; i<cuisines.size(); i++){
+            Log.d("ArrayList-cuisines", cuisines.get(i).getCuisine_name());
+        }
+
 
         //TODO: get the preferred cuisines of the current user
 
@@ -113,7 +183,7 @@ public class RestaurantRecommendationActivity extends AppCompatActivity {
                 }
             };
 
-            RequestQueueSingleton.getmInstance(getApplicationContext()).addToRequestQueue (restaurantRequest);
+//            RequestQueueSingleton.getmInstance(getApplicationContext()).addToRequestQueue (restaurantRequest);
 
         }
 
--- app/src/main/java/com/enroute/enroute/model/Cuisine.java
+++ app/src/main/java/com/enroute/enroute/model/Cuisine.java
@@ -2,19 +2,19 @@ package com.enroute.enroute.model;
 
 public class Cuisine {
 
-    public String cuisine_id;
+    public int cuisine_id;
     public String cuisine_name;
 
     public Cuisine(){
 
     }
 
-    public Cuisine(String cuisine_id, String cuisine_name) {
+    public Cuisine(int cuisine_id, String cuisine_name) {
         this.cuisine_id = cuisine_id;
         this.cuisine_name = cuisine_name;
     }
 
-    public String getCuisine_id() {
+    public int getCuisine_id() {
         return cuisine_id;
     }
 
@@ -22,7 +22,7 @@ public class Cuisine {
         return cuisine_name;
     }
 
-    public void setCuisine_id(String cuisine_id) {
+    public void setCuisine_id(int cuisine_id) {
         this.cuisine_id = cuisine_id;
     }
 
