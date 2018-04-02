d1e76d4ec632bb1e2ae5cf7d9ffcc9daa4d765f5
youranzhang <yr994423@dal.ca>
2018-04-02 11:49:37 -0300

    user model changed

--- app/src/main/java/com/enroute/enroute/model/User.java
+++ app/src/main/java/com/enroute/enroute/model/User.java
@@ -11,7 +11,7 @@ public class User {
     public String uphone;
     public String uemail;
     public String upassword;
-
+    public String uid;
 
     //user String to present preference
     public String upreference;
@@ -20,7 +20,9 @@ public class User {
 
     public User() {
     }
-    public User(String uname, String uphone, String upreference, String uemail, String upassword) {
+
+    public User(String uid,String uname, String uphone, String upreference, String uemail, String upassword) {
+        this.uid=uid;
         this.uname=uname;
         this.uphone=uphone;
         this.uemail=uemail;
@@ -28,51 +30,49 @@ public class User {
         this.upreference=upreference;
     }
 
+    public String getUid() {
+        return uid;
+    }
 
     public String getUname() {
         return uname;
     }
 
-
     public String getUphone() {
         return uname;
     }
-
 
     public String getUemail() {
         return uname;
     }
 
-
     public String getUpassword() {
         return upassword;
     }
-
 
     public String getUpreference() {
         return upreference;
     }
 
+    public void setUid(String uid){
+        this.uid=uid;
+    }
 
     public void setUname(String uname){
         this.uname=uname;
     }
 
-
     public void setUphone(String uphone){
         this.uphone=uphone;
     }
-
 
     public void setUemail(String uemail){
         this.uemail=uemail;
     }
 
-
     public void setUpassword(String upassword){
         this.upassword=upassword;
     }
-
 
     public void setUpreference(String upreference){
         this.upreference=upreference;
